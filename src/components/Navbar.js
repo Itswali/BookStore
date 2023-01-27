d898db6729a4e99a8f511a6465ac101ee3f62f9b
Wali Muhammad <luckywali0986@gmail.com>
2023-01-27 12:35:41 +0500

    add styling for header nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,28 @@
+/* eslint-disable react/void-dom-elements-no-children */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/style.scss';
+import logo from '../icon.png';
 
 const Navbar = () => (
   <div>
     <nav className="navbar">
       <h1>Bookstore CMS</h1>
       <ul className="nav-links">
-        <li>
+        <li className="link-link">
           {' '}
-          <Link to="/">Books</Link>
+          <Link to="/" className="link-linked">Books</Link>
           {' '}
         </li>
-        <li>
+        <li className="link-link">
           {' '}
-          <Link to="/Category">Cartegory</Link>
+          <Link to="/Category" className="link-linked">Cartegory</Link>
           {' '}
         </li>
       </ul>
+      <button type="button" className="icon-button">
+        <img className="icon" src={logo} alt="person" />
+      </button>
     </nav>
   </div>
 );
